9a595f6dc6a495bde091eff7b5ed7dafc77580f3
Uma Sankar Chintapalli <umasankarreddych@gmail.com>
2025-05-06 14:33:47 +0530

    modified terraform file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,29 +1,25 @@
-provider "aws" {
-region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "s3Bucket" {
-     bucket = "my-terraform-with-cicd"
-     acl       = "public-read"
+  bucket = "my-terraform-with-cicd"
 
-     policy  = <<EOF
+  # Bucket policy to allow public read access to all objects
+  policy = <<EOF
 {
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::my-terraform-with-cicd/*",
-         "principal" : "*"
-      }
-    ]
-  }
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::my-terraform-with-cicd/*"
+    }
+  ]
+}
 EOF
 
-   website {
-       index_document = "index.html"
-   }
+  # Static website configuration
+  website {
+    index_document = "index.html"
+    # Optional: Set an error document if desired
+    # error_document = "error.html"
+  }
 }
