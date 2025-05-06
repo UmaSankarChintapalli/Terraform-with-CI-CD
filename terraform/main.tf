28239597f4566b00712e6be5bdbe809341e9ee76
Uma Sankar Chintapalli <umasankarreddych@gmail.com>
2025-05-06 14:52:57 +0530

    changed terraform main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,38 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create the S3 bucket
-resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "my-terraform-with-cicd"
-  acl    = null
-
-  website {
-    index_document = "index.html"
-  }
-
-  force_destroy = true  # Optional: allows auto-deleting all objects if you destroy the bucket
-}
-
-# Set object ownership to BucketOwnerEnforced
-resource "aws_s3_bucket_ownership_controls" "ownership" {
-  bucket = aws_s3_bucket.s3Bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-# Disable S3 block public access
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.s3Bucket.id
-
-  block_public_acls       = false
-  ignore_public_acls      = false
-  block_public_policy     = false
-  restrict_public_buckets = false
-}
-
-# Attach a public-read policy using a separate resource
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.s3Bucket.id
 
@@ -49,4 +17,8 @@ resource "aws_s3_bucket_policy" "public_policy" {
       }
     ]
   })
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
 }
